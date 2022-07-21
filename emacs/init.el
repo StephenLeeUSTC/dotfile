382aca70b2ee8b74b1151b7d446b73275bd274a6
Dan <stephen.lee.ustc@gmail.com>
2022-07-21 21:29:16 +0800

    use org mode to refactor emacs config, for fun

--- emacs/init.el
+++ emacs/init.el
@@ -1,16 +1,24 @@
-(add-to-list 'load-path "~/.emacs.d/lisp/")
+;;; package --- Summary:
 
-(defun open-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
+;;; Commentary:
+;; "Use org to load config from ~/.dotfiles/emacs/config.org.
+;; First we'll need the org package.  Use M-x install-package org if you do not
+;; already have it Ensure that all config files are in the correct dir.
+;; This file can then be symlinked to your home directory.  Everything after
+;; ';;; init.el ends here' is auto-inserted by Emacs via customize-variable.
+;; If you don't like those lazy configs just delete everything after the ends
+;; here line"
 
+;;; Use a custom variables file. Load these variables first.
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
 
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-defaults)
-(require 'init-org)
-(require 'init-keybindings)
+;; Load config.el if it exists. If it does not, tangle the configuration file
+;; and prompt the user to restart. By using this method, we remove any need
+;; to (require 'org) so early in the configuration.n
 
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(org-babel-load-file "~/.config/emacs/config.org")
 
-(load-file custom-file)
+(load custom-file 'noerror 'nomessage)
+
+(provide 'emacs)
+;;; init.el ends here
